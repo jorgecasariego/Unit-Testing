07aa7f473576043a1e8947acdebc0fa6bef085fe
Jorge Casariego <jorgekasa@gmail.com>
2019-10-23 00:37:52 -0300

    Configuration of project

---
+++ app/src/test/java/com/jorge/casariego/unittesting/models/NoteTest.java
@@ -0,0 +1,36 @@
+package com.jorge.casariego.unittesting.models;
+
+import org.junit.jupiter.api.Test;
+
+public class NoteTest {
+    /**
+     *  Compare two notes are equals
+     *
+     *  Continue with vid 6
+     */
+    @Test
+    void isNotesEquals_identicalProperties() throws Exception{
+        // Arrange
+
+        //Acts
+
+        //Asserts
+    }
+
+    /**
+     *  Compare two notes with 2 differentes ids
+     */
+
+    /**
+     * Compare two notes with 2 differentes timestamp
+     */
+
+    /**
+     * Compare two notes with differents title
+     */
+
+    /**
+     * Compare two notes with differents contents
+     */
+
+}
